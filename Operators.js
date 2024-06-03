28508cbe7755759e5ea91bec335635d3d66b7562
Alumno370GC <alumnoitleon@gmail.com>
2024-06-03 05:41:31 -0600

    Operator

---
+++ Operators.js
@@ -0,0 +1,21 @@
+let x = 5;
+//adition operator
+console.log("Addition: x +3 ", x +3);
+//subtration operator
+console.log("Subtration: x +3 ", x - 3);
+//Multiplication
+console.log("Multiplication: x *3 ", x *3 );
+
+//division operator
+console.log("Division: x / 3", x/3);
+
+//remaider operator
+console.log ("Remider: x % 3",  x % 3);
+
+
+
+
+
+
+
+
