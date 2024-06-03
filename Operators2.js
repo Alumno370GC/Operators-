16a676fbd29b39461988c5a0c1a5350dbf209c23
Alumno370GC <alumnoitleon@gmail.com>
2024-06-03 06:22:25 -0600

    Operators2

---
+++ Operators2.js
@@ -0,0 +1,43 @@
+let  a = 7;
+console.log(a);
+
+//addition
+a += 5; // a = a + 5;
+console.log(a);
+
+//Subtraction
+a -= 5; // a = a - 5;
+console.log(a);
+
+//Multiplication
+a *= 2; // a = a * 2;
+console.log(a);
+
+//division
+a /= 2; // a = a / 2;
+console.log(a);
+
+
+// remainder 
+a %= 2; // a = a % 2;
+console.log(a);
+
+
+//exponentiation
+a **= 2; // a = a ** 2;
+console.log(a);
+
+
+
+const f=3, g =2;
+console.log(f > g);
+
+
+
+
+
+
+// 4. JavaScript Logical Operators
+
+const x = 5, y = 3;
+console.log (( x < 6)  && (y < 5));
